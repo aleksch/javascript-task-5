958fac00848224f468fffddb0008ab76d20ad538
Chernyshev <sanocher@gmail.com>
2017-11-01 12:33:05 +0500

    solution

--- emitter.js
+++ emitter.js
@@ -8,10 +8,25 @@ getEmitter.isStar = true;
 module.exports = getEmitter;
 
 /**
+ * @param {Object} events
+ * @param {String} eventName
+ */
+function callEvent(events, eventName) {
+    if (eventName in events) {
+        for (let event of events[eventName]) {
+            event.handler.call(event.context);
+        }
+    }
+}
+
+/**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+
+    let events = {};
+
     return {
 
         /**
@@ -19,26 +34,56 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} Emmiter
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!(event in events)) {
+                events[event] = [];
+            }
+
+            events[event].push({ context, handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} Emmiter
          */
         off: function (event, context) {
-            console.info(event, context);
+            let eventsNames = Object.keys(events)
+                .filter((e) => e === event || e.startsWith(event + '.'));
+
+            for (let eventName of eventsNames) {
+                events[eventName] = events[eventName].filter(
+                    curEvent => curEvent.context !== context
+                );
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} Emmiter
          */
         emit: function (event) {
-            console.info(event);
+
+            let eventNames = [];
+            let namespaceParts = event.split('.');
+            while (namespaceParts.length > 0) {
+                eventNames.push(namespaceParts.join('.'));
+                namespaceParts.pop();
+            }
+
+            for (let eventName of eventNames) {
+                callEvent(events, eventName);
+            }
+
+            return this;
         },
 
         /**
@@ -48,9 +93,18 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} Emmiter
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            let count = 0;
+            this.on(event, context, function () {
+                if (count < times) {
+                    handler.call(context);
+                }
+                count++;
+            });
+
+            return this;
         },
 
         /**
@@ -60,9 +114,18 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} Emmiter
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            let count = 0;
+            this.on(event, context, function () {
+                if (count % frequency === 0) {
+                    handler.call(context);
+                }
+                count++;
+            });
+
+            return this;
         }
     };
 }
